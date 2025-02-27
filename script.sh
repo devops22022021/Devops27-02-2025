d0daa121245ae1403afb4d821886b83bcda5f0af
shreya <shreyantasulebhavi1@gmail.com>
2025-02-27 15:02:24 +0530

    added script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Variables
+# - Static
+# - Dynamic
+# - Environmental
+course="Devops"
+echo "Enter your name: "
+read name
+
+echo "Hello $name  Welcome to $course Course at $place"
+
+
